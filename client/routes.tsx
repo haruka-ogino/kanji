5c5ca4c2114d452cb7f57047438a1319e00daf90
Haruka Ogino <harukariq@gmail.com>
2024-05-10 19:35:02 +1200

    add kanji route

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,9 +1,10 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import Home from './components/Home.tsx'
 import Layout from './components/Layout.tsx'
+import KanjiList from './components/KanjiList.tsx'
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
-    {/* <Route path="films" element={<Films />} /> */}
+    <Route path="kanji" element={<KanjiList />} />
   </Route>,
 )
