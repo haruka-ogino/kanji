820d290f786b82b9d2c9abcd4d46ed5254e59507
Haruka Ogino <harukariq@gmail.com>
2024-05-10 19:47:28 +1200

    create kanji component

---
+++ client/components/KanjiDisplay.tsx
@@ -0,0 +1,7 @@
+export default function KanjiList() {
+  return (
+    <>
+      <p>single kanji</p>
+    </>
+  )
+}
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,7 +1,7 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
-import Home from './components/Home.tsx'
-import Layout from './components/Layout.tsx'
-import KanjiList from './components/KanjiList.tsx'
+import Home from './pages/Home.tsx'
+import Layout from './pages/Layout.tsx'
+import KanjiList from './pages/KanjiList.tsx'
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
