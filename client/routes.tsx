e612469b9855d24d8d3c613a670cce9d27737f7b
Haruka Ogino <harukariq@gmail.com>
2024-05-10 19:33:34 +1200

    set up home and layout

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,3 +1,9 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
-import App from './components/App.tsx'
-export default createRoutesFromElements(<Route index element={<App />} />)
+import Home from './components/Home.tsx'
+import Layout from './components/Layout.tsx'
+export default createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+    {/* <Route path="films" element={<Films />} /> */}
+  </Route>,
+)
