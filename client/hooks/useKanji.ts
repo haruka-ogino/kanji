a80311e9defb8f3aac98baeb39cfdb09c7ce447b
Haruka Ogino <harukariq@gmail.com>
2024-05-11 19:19:15 +1200

    make api call to get my kanji

--- client/hooks/useKanji.ts
+++ client/hooks/useKanji.ts
@@ -1,8 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
-import { getKanjiList } from '../apis/kanji.ts'
+import { getKanjiList, getMyKanji } from '../apis/kanji.ts'
 
 export function useKanjiList() {
   const query = useQuery({ queryKey: ['kanji'], queryFn: getKanjiList })
+  return {
+    ...query,
+  }
+}
+
+export function useMyKanji() {
+  const query = useQuery({ queryKey: ['kanji'], queryFn: getMyKanji })
   return {
     ...query,
   }
