a6165d843ce155d282c3e7ac740dc98eda9d0433
Haruka Ogino <harukariq@gmail.com>
2024-05-11 19:31:17 +1200

    make call to single kanji on component

--- client/components/KanjiBackside.tsx
+++ client/components/KanjiBackside.tsx
@@ -1,11 +1,30 @@
+import { useKanjiInfo } from '../hooks/useKanji'
+
 interface Props {
   kanji: string
 }
 
 export default function KanjiDisplay({ kanji }: Props) {
-  return (
-    <div className="character">
-      <h2>{kanji}</h2>
-    </div>
-  )
+  const { data: character, isLoading, isError } = useKanjiInfo(kanji)
+
+  // To get the first n elements of an array, use
+
+  // const slicedArray = array.slice(0, n);
+
+  if (isLoading) {
+    return <p>is loading...</p>
+  }
+  if (isError) {
+    return <p>Something went wrong</p>
+  }
+
+  if (character) {
+    console.log(character)
+
+    return (
+      <div className="character">
+        <h2>{character}</h2>
+      </div>
+    )
+  }
 }
