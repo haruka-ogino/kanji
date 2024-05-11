fd19f34e70f60db3ea89e862ccddcd9f7603901a
Haruka Ogino <harukariq@gmail.com>
2024-05-11 19:18:31 +1200

    create kanji route FE

--- client/apis/kanji.ts
+++ client/apis/kanji.ts
@@ -8,7 +8,19 @@ export async function getKanjiList() {
 
     return res.body
   } catch (error) {
-    console.error('Error fetching kanji. list')
+    console.error('Error fetching kanji list')
     throw new Error('Failed to fetch kanji list. Please try again.')
+  }
+}
+
+const myRootUrl = '/api/v1'
+export async function getMyKanji() {
+  try {
+    const res = await request.get(myRootUrl + `/kanji`)
+
+    return res.body
+  } catch (error) {
+    console.error('Error fetching your kanji')
+    throw new Error('Failed to fetch your kanji. Please try again.')
   }
 }
