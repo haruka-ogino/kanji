881134ba6610ae292de4d3f41461542ea9d335c7
Haruka Ogino <harukariq@gmail.com>
2024-05-11 19:29:03 +1200

    make hook to obtain single kanji's info

--- client/hooks/useKanji.ts
+++ client/hooks/useKanji.ts
@@ -1,8 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
-import { getKanjiList, getMyKanji } from '../apis/kanji.ts'
+import { getKanjiInfo, getKanjiList, getMyKanji } from '../apis/kanji.ts'
 
 export function useKanjiList() {
   const query = useQuery({ queryKey: ['kanji'], queryFn: getKanjiList })
+  return {
+    ...query,
+  }
+}
+
+export function useKanjiInfo(character: string) {
+  const query = useQuery({
+    queryKey: ['kanji', character],
+    queryFn: () => getKanjiInfo(character),
+  })
   return {
     ...query,
   }
