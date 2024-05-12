e6866e90317eab5df043cf67043c4814aeec9c4d
Haruka Ogino <harukariq@gmail.com>
2024-05-12 14:53:37 +1200

    make flip card function

--- client/components/KanjiBackside.tsx
+++ client/components/KanjiBackside.tsx
@@ -13,6 +13,14 @@ export default function KanjiBackside({ kanji, setBack, i }: Props) {
 
   // const slicedArray = array.slice(0, n);
 
+  function flipCard() {
+    setBack((prevBack) => {
+      const newBack = [...prevBack]
+      newBack[i] = false
+      return newBack
+    })
+  }
+
   if (isLoading) {
     return <p>is loading...</p>
   }
@@ -23,10 +31,9 @@ export default function KanjiBackside({ kanji, setBack, i }: Props) {
   if (character) {
     console.log(character)
 
-    function flipCard() {}
     return (
       // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-      <div className="character" onClick={() => setBack(true)}>
+      <div className="character" onClick={flipCard}>
         <h2>{character.kanji}</h2>
         {character.meanings.map((meaning: string, i: number) => (
           <p key={i}>{meaning}</p>
